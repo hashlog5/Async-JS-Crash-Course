f4fdb283044ed8ceeb0f59531b52d49aaec3a6af
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-08-04 19:02:34 -0700

    async await

--- promises.js
+++ promises.js
@@ -33,16 +33,32 @@ function createPost(post) {
 //   .then(getPosts)
 //   .catch((err) => console.log(err));
 
-// Promise.all
-const promise1 = Promise.resolve('hello world');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-  setTimeout(resolve, 2000, 'goodbye')
-);
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(
-  (res) => res.json()
-);
+// async/await
+// async function init() {
+//   await createPost({ title: 'post three', body: 'this is post three' });
 
-Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
-  console.log(values)
-);
+//   getPosts();
+// }
+// init();
+
+// async/await with fetch
+async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  console.log(data);
+}
+fetchUsers();
+
+// Promise.all
+// const promise1 = Promise.resolve('hello world');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+//   setTimeout(resolve, 2000, 'goodbye')
+// );
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(
+//   (res) => res.json()
+// );
+
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+//   console.log(values)
+// );
